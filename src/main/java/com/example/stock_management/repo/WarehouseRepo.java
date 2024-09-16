d320d16f817dd95dad2ab45f2b76f091d228b10c
Zakariae Lajoui <lajoui.zakariae.1@gmail.com>
2024-09-16 22:26:49 +0100

    refactor: remove unused import

--- src/main/java/com/example/stock_management/repo/WarehouseRepo.java
+++ src/main/java/com/example/stock_management/repo/WarehouseRepo.java
@@ -2,12 +2,7 @@ package com.example.stock_management.repo;
 
 import com.example.stock_management.models.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface WarehouseRepo extends JpaRepository<Warehouse, Integer> {
-
-    List<Warehouse> findByName(String name);
 
 }
